5af36989af5a4cb080ee3221d2d9c92ef9ede0ec
ravigunz <ravigunz@gmail.com>
2023-08-03 11:48:22 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `my first gatsby website`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
